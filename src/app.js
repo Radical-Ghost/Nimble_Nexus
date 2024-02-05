ecd3703bb3740ceaf85fdc925e7e42d060628f7c
Soham Mandavkar <radicalghost98@gmail.com>
2024-02-06 00:15:53 +0530

    updated theme

--- src/app.js
+++ src/app.js
@@ -22,28 +22,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("", require("./routes/routes.js"));
 
-const routes = [
-	{ path: "/", view: "home" },
-	{ path: "/notice", view: "notice" },
-	{ path: "/department", view: "department" },
-	{ path: "/queries", view: "queries" },
-	{ path: "/about", view: "about" },
-	{ path: "/admin", view: "admin" },
-	{ path: "/dashboard", view: "dashboard" },
-	{ path: "/cs", view: "Departments/cs" },
-	{ path: "/aids", view: "Departments/aids" },
-	{ path: "/extc", view: "Departments/extc" },
-	{ path: "/fe", view: "Departments/fe" },
-	{ path: "/it", view: "Departments/it" },
-	{ path: "/mech", view: "Departments/mech" },
-];
-
-routes.forEach((route) => {
-	app.get(route.path, function (req, res) {
-		res.render(route.view);
-	});
-});
-
 app.listen(port, function () {
 	console.log(`Server is running on http://localhost:${port}`);
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/user', function(req, res) {
-  res.send('All User');
+const routes = [
+	{ path: "/", view: "home" },
+	{ path: "/notice", view: "notice" },
+	{ path: "/department", view: "department" },
+	{ path: "/queries", view: "queries" },
+	{ path: "/about", view: "about" },
+	{ path: "/admin", view: "admin" },
+	{ path: "/dashboard", view: "dashboard" },
+	{ path: "/cs", view: "Departments/cs" },
+	{ path: "/aids", view: "Departments/aids" },
+	{ path: "/extc", view: "Departments/extc" },
+	{ path: "/fe", view: "Departments/fe" },
+	{ path: "/it", view: "Departments/it" },
+	{ path: "/mech", view: "Departments/mech" },
+];
+
+routes.forEach((route) => {
+	router.get(route.path, function (req, res) {
+		res.render(route.view);
+	});
 });
 
 module.exports = router;
