4425fdae1eaa62dade3404a709a97b5e29b4fc69
Soham Mandavkar <radicalghost98@gmail.com>
2024-02-05 18:17:25 +0530

    Updated src/app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
@@ -34,7 +36,6 @@ routes.forEach((route) => {
 	});
 });
 
-const port = 3000;
 app.listen(port, function () {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
