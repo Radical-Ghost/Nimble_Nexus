4f56c8686637b7eea30426ca1f3ae4b5c3bf17b9
Soham Mandavkar <radicalghost98@gmail.com>
2024-02-08 00:05:02 +0530

    .

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,24 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const routes = [
-	{ path: "/", view: "home" },
-	{ path: "/notice", view: "notice" },
-	{ path: "/department", view: "department" },
-	{ path: "/queries", view: "queries" },
-	{ path: "/about", view: "about" },
-	{ path: "/admin", view: "admin" },
-	{ path: "/dashboard", view: "dashboard" },
-	{ path: "/cs", view: "Departments/cs" },
-	{ path: "/aids", view: "Departments/aids" },
-	{ path: "/extc", view: "Departments/extc" },
-	{ path: "/fe", view: "Departments/fe" },
-	{ path: "/it", view: "Departments/it" },
-	{ path: "/mech", view: "Departments/mech" },
+	{ path: "/", view: "home", title: "Home" },
+	{ path: "/notice", view: "notice", title: "Notice" },
+	{ path: "/department", view: "department", title: "Department" },
+	{ path: "/queries", view: "queries", title: "Queries" },
+	{ path: "/about", view: "about", title: "About" },
+	{ path: "/admin", view: "admin", title: "Admin" },
+	{ path: "/dashboard", view: "dashboard", title: "Dashboard" },
+	{ path: "/cs", view: "Departments/cs", title: "Departments Cs" },
+	{ path: "/aids", view: "Departments/aids", title: "Departments Aids" },
+	{ path: "/extc", view: "Departments/extc", title: "Departments Extc" },
+	{ path: "/fe", view: "Departments/fe", title: "Departments Fe" },
+	{ path: "/it", view: "Departments/it", title: "Departments It" },
+	{ path: "/mech", view: "Departments/mech", title: "Departments Mech" },
 ];
 
 routes.forEach((route) => {
 	router.get(route.path, function (req, res) {
-		res.render(route.view);
+		res.render(route.view, { title: route.title });
 	});
 });
 
