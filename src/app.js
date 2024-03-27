963161500205cc560343480c193d1035d36e3db9
Soham Mandavkar <radicalghost98@gmail.com>
2024-03-27 22:33:14 +0530

    .

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const store = new MongoDBsession({
 	uri: URI,
