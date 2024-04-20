64833a8e38b33de087adbcf8a86ddef848648890
Soham Mandavkar <radicalghost98@gmail.com>
2024-04-20 21:54:10 +0530

    update 20-04-2024

--- src/models/query.js
+++ src/models/query.js
@@ -1,10 +1,15 @@
+const { name } = require("ejs");
 const mongoose = require("mongoose");
 
-const querySchema = new mongoose.Schema({
+const replySchema = new mongoose.Schema({
 	userId: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true,
 		ref: "User",
+	},
+	parentReplyId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Query",
 	},
 	text: {
 		type: String,
@@ -19,6 +24,36 @@ const querySchema = new mongoose.Schema({
 		default: 0,
 	},
 	replies: [this],
+});
+
+const querySchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	noticeId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "Notice",
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	text: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+	likes: {
+		type: Number,
+		default: 0,
+	},
+	replies: [replySchema],
 });
 
 module.exports = mongoose.model("Query", querySchema);
